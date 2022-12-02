21ab6f944fa768fabcc119c5a11981dc2ad6d4b8
Yevgeniy <yevgeniy.tolks@gmail.com>
2022-12-02 15:28:37 +0200

    token generation implementation

---
+++ src/main/java/com/example/eBookingAppointment/registration/token/ConfirmationToken.java
@@ -0,0 +1,52 @@
+package com.example.eBookingAppointment.registration.token;
+
+import com.example.eBookingAppointment.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class ConfirmationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(
+            nullable = false
+    )
+    private String token;
+    @Column(
+            nullable = false
+    )
+    private LocalDateTime dateCreated;
+    @Column(
+            nullable = false
+    )
+    private LocalDateTime dateExpiredAt;
+
+    @ManyToOne
+    @JoinColumn(
+            nullable = false,
+            name = "user_id"
+    )
+    private User user;
+
+    private LocalDateTime confirmedAt;
+
+    public ConfirmationToken(String token,
+                             LocalDateTime dateCreated,
+                             LocalDateTime dateExpiredAt,
+                             User user) {
+        this.token = token;
+        this.dateCreated = dateCreated;
+        this.dateExpiredAt = dateExpiredAt;
+        this.user = user;
+    }
+}
